57b6ceea969f34ed6b2fe845066302255e2c518d
NEngland24 <824864@mystma.org>
2023-03-31 15:04:58 -0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
 def factorial(x):
-    for i in range(0,(x+1)):
-        return x * i 
+    f = 1
+    for i in range(1, x+1):
+        f = i * f
+        print(f)
+factorial(5)
